b971c8b089bbb1ea7b396c31d4b2dee6b63fcf19
Zytha <yangyong080228@gmail.com>
2022-04-27 22:03:22 +0800

    🔥 First Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            isGood: false,
+            likes: 0,
+            text: 'Bruh',
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>📝{{ text }}</h1>
+    <div v-if="isGood">
+        <h1>👍 Good</h1>
+    </div>
+    <div v-else>
+        <h1>👎 Bad</h1>
+    </div>
+    <button v-on:click="isGood = !isGood">✨Switch</button>
+    <button v-on:click="likes++">👍 {{ likes }}</button>
+    <input v-model="text">
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
